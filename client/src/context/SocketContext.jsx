1d380bb6ba93b9ec3269c6353d0ac96e65823880
ritik <chaurasiyaritik37@gmail.com>
2026-05-13 22:23:04 +0530

    fixed deployment

--- client/src/context/SocketContext.jsx
+++ client/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
 	useEffect(() => {
 		if (authUser) {
-			const socket = io("http://localhost:5000", {
+			const socket = io("https://my-chat-app-bttn.onrender.com", {
 				query: {
 					userId: authUser._id,
 				},
